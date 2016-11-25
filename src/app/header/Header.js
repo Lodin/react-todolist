8150b3d128b46c7eebf0594b8243930dac314e14
Vlad Rindevich <rindevich.vs@gmail.com>
2016-11-26 02:37:12 +0300

    refactor(): update Header component

--- src/app/header/Header.js
+++ src/app/header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import ActiveFilter from './filter/ActiveFilter';
-import Search from './filter/Search';
+import ActiveFilter from './ActiveFilter';
 import ProgressBar from './ProgressBar';
+import Search from './Search';
 import styles from './Header.scss';
 
 const Header = () => (
   <div className={styles.container}>
-    <div className={styles.main}>
-      <div className={styles.logo}>To-Do List</div>
+    <div className={styles.row}>
+      <div className={styles.title}>To-Do List</div>
       <div className={styles.filter}>
         <ActiveFilter />
         <Search />
       </div>
     </div>
-    <ProgressBar completed="50" />
+    <ProgressBar completed={50} />
   </div>
 );
 
