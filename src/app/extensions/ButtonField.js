9164e07853e300fac3485163c529688425a2d3a5
Vlad Rindevich <rindevich.vs@gmail.com>
2016-11-26 01:38:38 +0300

    feat(app): add ButtonField extension component

---
+++ src/app/extensions/ButtonField.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import styles from './ButtonField.scss';
+
+/**
+ * @param props {name, onAdd}
+ */
+class ButtonField extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {value: ''};
+    this.onInput = ::this.onInput;
+    this.onAdd = ::this.onAdd;
+  }
+
+  onInput(event) {
+    this.setState({value: event.target.value});
+  }
+
+  onAdd() {
+    this.props.onAdd(this.state.value);
+    this.setState({value: ''});
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <div>
+          <TextField
+            hintText={this.props.placeholder}
+            name={this.props.name}
+            value={this.state.value}
+            onChange={this.onInput} />
+        </div>
+        <div>
+          <FlatButton
+            label={this.props.label}
+            onClick={this.onAdd} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ButtonField;
