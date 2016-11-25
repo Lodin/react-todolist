ecb620c087d0870b7675390ed748f69e41c407cd
Vlad Rindevich <rindevich.vs@gmail.com>
2016-11-26 01:36:43 +0300

    refactor(): remove categories in favor of tasks

--- src/store/categories/actions.js
+++
@@ -1,6 +0,0 @@
-export const CATEGORY_ADD = 'CATEGORY_ADD';
-export const CATEGORY_ADD_SUB = 'CATEGORY_ADD_SUB';
-export const CATEGORY_EDIT = 'CATEGORY_EDIT';
-export const CATEGORY_REMOVE = 'CATEGORY_ADD';
-export const CATEGORY_ADD = 'CATEGORY_ADD';
-export const CATEGORY_ADD = 'CATEGORY_ADD';
