e77cdf8d9cc961329991bf9f54980958cb1581b8
Vlad Rindevich <rindevich.vs@gmail.com>
2016-11-26 01:38:23 +0300

    feat(app): add ClearableTextField extension component

---
+++ src/app/extensions/ClearableTextField.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import IconButton from 'material-ui/IconButton'
+import TextField from 'material-ui/TextField';
+import CancelIcon from 'material-ui/svg-icons/navigation/cancel';
+import {isEnterKey} from '../../utils/checkers';
+import styles from './ClearableTextField.scss';
+
+/**
+ * @param props {name, onEnter, onCancel}
+ */
+class ClearableTextField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {search: ''};
+
+    this.onInput = ::this.onInput;
+    this.onCancel = ::this.onCancel;
+  }
+
+  onInput(event) {
+    this.setState({search: event.target.value});
+  }
+
+  onCancel() {
+    this.setState({search: ''});
+    this.props.onCancel();
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <TextField
+          name={this.props.name}
+          value={this.state.search}
+          onChange={this.onInput}
+          onKeyUp={
+            event =>
+              isEnterKey(event.keyCode)
+                ? this.props.onEnter(this.state.search)
+                : null
+          }
+        />
+        <IconButton tooltip="Cancel" onClick={this.onCancel}>
+          <CancelIcon />
+        </IconButton>
+      </div>
+    );
+  }
+}
+
+export default ClearableTextField;
